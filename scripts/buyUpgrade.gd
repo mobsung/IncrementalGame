b49bb95568eba88533fc9dd99259f397f374a2b0
mobsung <movileanu.marcel2001@gmail.com>
2026-07-08 22:21:39 +0200

    added global multipliers

--- scripts/buyUpgrade.gd
+++ scripts/buyUpgrade.gd
@@ -5,7 +5,7 @@ signal was_pressed
  
 @export var upgrade_cost: int = 1
 @export var upgrade_name: Global_data.LVL1UpgradeType
-@export var next_upgrade_cost: int = 1
+@export var growth_factor: float = 1.05
 @onready var parent_counter: CounterComponent = owner
 
 var upgrade_level: int = 0
@@ -46,7 +46,7 @@ func _on_pressed() -> void:
 	
 	if approved == true and not max_level_reached:
 		upgrade_level += 1
-		upgrade_cost += next_upgrade_cost
+		upgrade_cost = int(upgrade_cost * pow(growth_factor, upgrade_level))
 		was_pressed.emit()
 		_on_currency_got(CurrencyManager.current_currency)
 		_update_ui()
--- scripts/currencyManager.gd
+++ scripts/currencyManager.gd
@@ -3,9 +3,11 @@ extends Node
 signal currency_changed(new_amount: int)
 
 var current_currency: int = 10000
+var presige_multiplier: float = 2.0
+var global_multiplier: float = 1.0
 
 func add_currency(amount: int ) -> void:
-	current_currency += amount
+	current_currency += (amount * presige_multiplier * global_multiplier)
 	currency_changed.emit(current_currency)
 
 func spend_currency(cost: int) -> bool:
